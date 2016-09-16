04758bb77a03e6078b368418cd46982b0acecfe4
Wajdi Gharsalli <wajdi.gharsalli@polymtl.ca>
2016-09-16 14:00:44 -0400

    wajdi merge

--- Cadriciel/Sources/DLL/Interface/FacadeInterfaceNative.h
+++ Cadriciel/Sources/DLL/Interface/FacadeInterfaceNative.h
@@ -21,13 +21,6 @@ extern "C" {
 	__declspec(dllexport) int obtenirAffichagesParSeconde();
 	__declspec(dllexport) bool executerTests();
 
-	//Click
-	__declspec(dllexport) void clickStart(int x, int y);
-	__declspec(dllexport) void clickCurrent(int x, int y);
-	__declspec(dllexport) void clickEnd(int x, int y);
-
-
-	__declspec(dllexport) void ajouterBonus(int x, int y);
 }
 
 #endif // __FACADE_INTERFACE_NATIVE_H__
