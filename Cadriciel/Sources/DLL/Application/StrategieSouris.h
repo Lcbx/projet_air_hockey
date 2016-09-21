673155dc9aca28b720268ed45cc7c21d9b67ad28
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2016-09-20 21:11:38 -0400

    erreur fixées, en voici de nouvelles

--- Cadriciel/Sources/DLL/Application/StrategieSouris.h
+++ Cadriciel/Sources/DLL/Application/StrategieSouris.h
@@ -23,22 +23,4 @@ private:
 	int X1, Y1, X2, Y2;							//pour garder l'information des points du Click
 };
 
-bool StrategieSouris::plusDe3px() {
-	return (X1 - X2)*(X1 - X2) + (Y1 - Y2)*(Y1 - Y2) > 9;
-}
-
-void StrategieSouris::start(int x, int y) {
-	//initialisation du clic
-	X1 = x; Y1 = y;
-	X2 = x; Y2 = y;
-}
-
-void StrategieSouris::current(int x, int y) {
-	X2 = x; Y2 = y;
-}
-
-void StrategieSouris::end(int x, int y) {
-	X2 = x; Y2 = y;
-	//opération
-	plusDe3px() ? operationDragClick() : operationShortClick();
 }
