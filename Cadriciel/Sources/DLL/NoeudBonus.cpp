574df991adf12f79e50d652282cf75a7f653837a
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2016-09-26 11:20:45 -0400

    selectionner un bonus change sa couleur :)

--- Cadriciel/Sources/DLL/NoeudBonus.cpp
+++ Cadriciel/Sources/DLL/NoeudBonus.cpp
@@ -75,8 +75,17 @@ void NoeudBonus::afficherConcret(const glm::mat4& vueProjection) const
 	// Recentrage du cube.
 	modele = glm::translate(modele, glm::vec3(0, 0, -10));
 	
-	// Affichage du modèle.
-	vbo_->dessiner(vueProjection* modele);
+	//change la couleur selon si est selectionne
+	if (estSelectionne()) {
+		// On active le mode XOR.
+		glEnable(GL_COLOR_LOGIC_OP);
+		glLogicOp(GL_XOR);
+		// Affichage du modèle.
+		vbo_->dessiner(vueProjection* modele);
+		//on désactive le mode XOR
+		glDisable(GL_COLOR_LOGIC_OP);
+	}
+	else vbo_->dessiner(vueProjection* modele);
 
 }
 
