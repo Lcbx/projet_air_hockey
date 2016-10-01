85ac0a6d0acc05557f5f7d1f588954ad2f0253d3
Wajdi Gharsalli <wajdi.gharsalli@polymtl.ca>
2016-10-01 19:52:06 -0400

    creation strategie Mise Echelle

---
+++ Cadriciel/Sources/DLL/MiseEchelle.h
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file MiseEchelle.h
+/// @author Wajdi Gharsalli	
+/// @date 2016-09-30
+/// @version 1.0
+///
+/// @addtogroup inf2990 INF2990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "StrategieSouris.h"
+#include "../VisiteurMiseEchelle.h"
+
+class MiseEchelle : public StrategieSouris {
+public:
+	//void start(int x, int y);				//les coordonnees du mouse_down
+	void current(int x, int y);				//les coordonnees du mouse_move
+											//void end(int x, int y);				//les coordonnees du mouse_up
+											//void position(int x, int y);			//position de la souris hors d'un click
+	void operationShortClick();				//lance l'opération adéquate selon le scénario du click
+	void operationDragClick();				//lance l'opération adéquate selon le scénario du click
+private:
+	//int X1, Y1, X2, Y2;					//pour garder l'information des points du Click
+	VisiteurMiseEchelle visiteur_;
+};
