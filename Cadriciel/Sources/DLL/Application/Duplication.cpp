5c0c7c68376fbc42b72817f9e8ac251939fa9cce
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2016-10-05 10:21:36 -0400

    commentaires dans les cpp d'application

--- Cadriciel/Sources/DLL/Application/Duplication.cpp
+++ Cadriciel/Sources/DLL/Application/Duplication.cpp
@@ -13,18 +13,54 @@
 #include "FacadeModele.h"
 
 
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn Duplication::start(int x, int y)
+///
+/// debute la duplication en dédoublant les objets selectionnés
+/// avec le visiteur duplication autour du curseur
+///
+/// @return Aucune.
+///
+/////////////////////////////////////////////////////////////////////////
 void Duplication::start(int x, int y) {
 	glm::dvec3 ancPoint; FacadeModele::obtenirInstance()->obtenirVue()->convertirClotureAVirtuelle(x, y, ancPoint);
 	visiteur_.duplicate(ancPoint);
 }
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn Duplication::current(int x, int y)
+///
+/// actualise la position des clones en les déplaçant autour du curseur
+///
+/// @return Aucune.
+///
+/////////////////////////////////////////////////////////////////////////
 void Duplication::current(int x, int y) {
 	glm::dvec3 nouvPoint; FacadeModele::obtenirInstance()->obtenirVue()->convertirClotureAVirtuelle(x, y, nouvPoint);
 	visiteur_.actualise(nouvPoint);
 }
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn Duplication::end(int x, int y)
+///
+/// finit la duplication en laisssant les clones là ou ils sont
+///
+/// @return Aucune.
+///
+/////////////////////////////////////////////////////////////////////////
 void Duplication::end(int x, int y) {
 	visiteur_.finalise();
 }
-
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn Duplication::escEnfonce()
+///
+/// annule la duplication en cours
+///
+/// @return Aucune.
+///
+/////////////////////////////////////////////////////////////////////////
 void Duplication::escEnfonce() {
 	visiteur_.supprimerClones();
 }
