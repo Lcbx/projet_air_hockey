eaa7a128b7c1fc5beb8396a8575ac876784264df
Ali Nassour <ali.nassour@polymtl.ca>
2016-11-29 12:23:42 -0500

    affichage FTGL en couleur

--- Cadriciel/Sources/DLL/Application/FacadeModele.cpp
+++ Cadriciel/Sources/DLL/Application/FacadeModele.cpp
@@ -492,6 +492,9 @@ void FacadeModele::activerCompteur(float temps)
 		chrono_ = formatHeure + ":" + formatMinute + ":" + formatSeconde;
 		//cout << chrono_ << endl;
 	}
+
+	//TODO -- mettre le compteur en pause quand on pese sur ESC
+	// idea - sauvegarder les valeurs actuelle et l'afficher tout le temps
 		
 }
 ////////////////////////////////////////////////////////////////////////
