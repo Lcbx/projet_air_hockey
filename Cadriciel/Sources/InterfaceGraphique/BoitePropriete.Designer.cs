5ef631a390d99a150ef977dca1220db1da0e4a16
Carl-Vincent <carl.vincent>
2016-12-01 15:55:32 -0500

    Correction terminologie boite propriété

--- Cadriciel/Sources/InterfaceGraphique/BoitePropriete.Designer.cs
+++ Cadriciel/Sources/InterfaceGraphique/BoitePropriete.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoitePropriete));
-            this.uniteAccelelration = new System.Windows.Forms.Label();
             this.labelAccélération = new System.Windows.Forms.Label();
             this.labelRebond = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -46,40 +45,29 @@
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // uniteAccelelration
-            // 
-            this.uniteAccelelration.AutoSize = true;
-            this.uniteAccelelration.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uniteAccelelration.Location = new System.Drawing.Point(12, 99);
-            this.uniteAccelelration.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.uniteAccelelration.Name = "uniteAccelelration";
-            this.uniteAccelelration.Size = new System.Drawing.Size(43, 13);
-            this.uniteAccelelration.TabIndex = 7;
-            this.uniteAccelelration.Text = "(m/s2  )";
-            // 
             // labelAccélération
             // 
             this.labelAccélération.AutoSize = true;
             this.labelAccélération.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.labelAccélération.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.labelAccélération.Location = new System.Drawing.Point(12, 83);
+            this.labelAccélération.Location = new System.Drawing.Point(10, 85);
             this.labelAccélération.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelAccélération.Name = "labelAccélération";
-            this.labelAccélération.Size = new System.Drawing.Size(74, 15);
+            this.labelAccélération.Size = new System.Drawing.Size(112, 15);
             this.labelAccélération.TabIndex = 6;
-            this.labelAccélération.Text = "Accélération";
+            this.labelAccélération.Text = "Accélération(m/s²): ";
             // 
             // labelRebond
             // 
             this.labelRebond.AutoSize = true;
             this.labelRebond.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.labelRebond.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.labelRebond.Location = new System.Drawing.Point(12, 57);
+            this.labelRebond.Location = new System.Drawing.Point(64, 60);
             this.labelRebond.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelRebond.Name = "labelRebond";
-            this.labelRebond.Size = new System.Drawing.Size(51, 15);
+            this.labelRebond.Size = new System.Drawing.Size(57, 15);
             this.labelRebond.TabIndex = 5;
-            this.labelRebond.Text = "Rebond";
+            this.labelRebond.Text = "Rebond: ";
             // 
             // label1
             // 
@@ -96,12 +84,12 @@
             this.labelFriction.AutoSize = true;
             this.labelFriction.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.labelFriction.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.labelFriction.Location = new System.Drawing.Point(12, 32);
+            this.labelFriction.Location = new System.Drawing.Point(71, 35);
             this.labelFriction.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelFriction.Name = "labelFriction";
-            this.labelFriction.Size = new System.Drawing.Size(47, 15);
+            this.labelFriction.Size = new System.Drawing.Size(50, 15);
             this.labelFriction.TabIndex = 3;
-            this.labelFriction.Text = "Friction";
+            this.labelFriction.Text = "Friction:";
             // 
             // numericUpDown3
             // 
@@ -112,7 +100,7 @@
             0,
             0,
             65536});
-            this.numericUpDown3.Location = new System.Drawing.Point(85, 80);
+            this.numericUpDown3.Location = new System.Drawing.Point(126, 83);
             this.numericUpDown3.Margin = new System.Windows.Forms.Padding(2);
             this.numericUpDown3.Maximum = new decimal(new int[] {
             10,
@@ -132,7 +120,7 @@
             0,
             0,
             65536});
-            this.numericUpDown2.Location = new System.Drawing.Point(85, 53);
+            this.numericUpDown2.Location = new System.Drawing.Point(126, 58);
             this.numericUpDown2.Margin = new System.Windows.Forms.Padding(2);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             1,
@@ -152,7 +140,7 @@
             0,
             0,
             65536});
-            this.numericUpDown1.Location = new System.Drawing.Point(85, 28);
+            this.numericUpDown1.Location = new System.Drawing.Point(126, 33);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(60, 21);
@@ -161,25 +149,24 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.numericUpDown3);
-            this.groupBox1.Controls.Add(this.uniteAccelelration);
             this.groupBox1.Controls.Add(this.labelFriction);
             this.groupBox1.Controls.Add(this.numericUpDown1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.labelAccélération);
             this.groupBox1.Controls.Add(this.labelRebond);
             this.groupBox1.Controls.Add(this.numericUpDown2);
-            this.groupBox1.Location = new System.Drawing.Point(147, 37);
+            this.groupBox1.Location = new System.Drawing.Point(129, 37);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(175, 155);
+            this.groupBox1.Size = new System.Drawing.Size(202, 127);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Configurations";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(129, 219);
+            this.button2.Location = new System.Drawing.Point(129, 194);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(98, 23);
             this.button2.TabIndex = 11;
             this.button2.Text = "Annuler";
             this.button2.UseVisualStyleBackColor = true;
@@ -187,11 +174,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(247, 219);
+            this.button3.Location = new System.Drawing.Point(233, 194);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(98, 23);
             this.button3.TabIndex = 12;
-            this.button3.Text = "Modifier";
+            this.button3.Text = "Sauvegarder";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -219,8 +206,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label uniteAccelelration;
         private System.Windows.Forms.Label labelAccélération;
         private System.Windows.Forms.Label labelRebond;
         private System.Windows.Forms.Label label1;
